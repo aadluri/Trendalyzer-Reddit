8657fdf831384e24c6671691e1c67341d82abdad
aadluri <a.adluri@gmail.com>
2025-11-04 21:34:04 -0800

    Some more initial files added and modified later

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# API creds, make a note to set these in .env file
+# API creds, make a note to set these in .env file, there is a blank .env file for anyone looking
 REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
 USER_AGENT = os.getenv("USER_AGENT", "reddit_trend_analyzer")
@@ -11,8 +11,8 @@ USER_AGENT = os.getenv("USER_AGENT", "reddit_trend_analyzer")
 # Database path - need to run scripts to create this
 DB_PATH = os.getenv("DB_PATH", "db/reddit_trends.db")
 
-# Subreddit list
+# Subreddit list - can change, but these are home decor related
 SUBREDDITS = ["HomeDecorating", "InteriorDesign", "DesignMyRoom", "CozyPlaces", "interiordecorating"]
 
-# too many posts may lead to rate , i would like 30-40
+# too many posts may lead to rate limiting, ideally would use 30-40
 POST_LIMIT = 10
